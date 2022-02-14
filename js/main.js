8de2b526bd20b36b73e252b73d3e8a02f3155af5
svetlana-urasinova <urasinova.work@gmail.com>
2022-02-14 17:50:43 +0100

    Add slider mobile settings

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const swiper = new Swiper('.main-slider', {
-    slidesPerView: 2,
-    spaceBetween: 28,
+    slidesPerView: 1,
+    spaceBetween: 0,
     loop: true,
     navigation: {
         nextEl: '.main-slider__arrow',
-    }
+    },
+    breakpoints: {
+        400: {
+            slidesPerView: 2,
+            spaceBetween: 28
+        }
+    },
 });
